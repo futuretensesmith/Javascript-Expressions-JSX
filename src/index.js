f24e9d195323d01daac938000c93a52bf61cf958
Kevin Smith <futuretense@gmail.com>
2021-02-10 17:46:15 -0800

    Added expressions within HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Sleeth";
+const lName = "Malthus";
+
+//only expressions between {} within HTML ** No statements **
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}</h1>
+    <p>your lucky number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
